3c380475c27266cce2457a58a5db100791381152
Ayden Beeson <abeeson@csu.edu.au>
2015-08-03 18:13:22 +1000

    brighten and dim scripts - Hard coded IP and zone etc

---
+++ dim.sh
@@ -0,0 +1,60 @@
+timer="$1"
+echo -n -e "\x45\x00\x55" >/dev/udp/192.168.0.250/8899
+sleep 0.1
+echo -n -e "\x45\x00\x55" >/dev/udp/192.168.0.250/8899
+sleep 0.1
+echo -n -e "\x45\x00\x55" >/dev/udp/192.168.0.250/8899
+sleep 0.1
+echo -n -e "\x4E\x1B\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x1A\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x19\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x18\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x17\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x16\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x15\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x14\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x13\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x12\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x11\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x10\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x0E\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x0D\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x0C\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x0B\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x0A\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x09\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x08\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x07\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x06\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x05\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x04\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x03\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x4E\x02\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x46\x00\x55" >/dev/udp/192.168.0.250/8899
+sleep $timer
+echo -n -e "\x46\x00\x55" >/dev/udp/192.168.0.250/8899
