c846cc1e14da3e5e507897f296b814e1ee52d86e
jsloane <jsloane@csu.edu.au>
2015-08-05 20:39:50 +1000

    dim only for movies

--- playeractions.py
+++ playeractions.py
@@ -4,6 +4,7 @@ import subprocess,os,socket,time
 class MyPlayer(xbmc.Player) :
 
         def __init__ (self):
+            self.isMovie = 0
             xbmc.Player.__init__(self)
 
         def onPlayBackStarted(self):
@@ -33,23 +34,24 @@ class MyPlayer(xbmc.Player) :
             self.sendUdpCommand(self, '\x41\x00', 0.1)
 
         def dim(self, event):
-            timer = 0.2
-            messages = ['\x4E\x1B\x55','\x4E\x1A\x55','\x4E\x19\x55','\x4E\x18\x55','\x4E\x17\x55','\x4E\x16\x55','\x4E\x15\x55','\x4E\x14\x55','\x4E\x13\x55','\x4E\x12\x55','\x4E\x11\x55','\x4E\x10\x55','\x4E\x0E\x55','\x4E\x0D\x55','\x4E\x0C\x55','\x4E\x0B\x55','\x4E\x0A\x55','\x4E\x09\x55','\x4E\x08\x55','\x4E\x07\x55','\x4E\x06\x55','\x4E\x05\x55','\x4E\x04\x55','\x4E\x04\x55','\x4E\x04\x55']
-            for message in messages:
-                self.sendUdpCommand(self, message, timer)
+            self.isMovie = xbmc.getCondVisibility('VideoPlayer.Content(movies)')
+            if self.isMovie:
+                timer = 0.2
+                messages = ['\x4E\x1B\x55','\x4E\x1A\x55','\x4E\x19\x55','\x4E\x18\x55','\x4E\x17\x55','\x4E\x16\x55','\x4E\x15\x55','\x4E\x14\x55','\x4E\x13\x55','\x4E\x12\x55','\x4E\x11\x55','\x4E\x10\x55','\x4E\x0E\x55','\x4E\x0D\x55','\x4E\x0C\x55','\x4E\x0B\x55','\x4E\x0A\x55','\x4E\x09\x55','\x4E\x08\x55','\x4E\x07\x55','\x4E\x06\x55','\x4E\x05\x55','\x4E\x04\x55','\x4E\x04\x55','\x4E\x04\x55']
+                for message in messages:
+                    self.sendUdpCommand(self, message, timer)
 
         def brighten(self, event):
-            timer = 0.2
-            messages = ['\x4E\x04\x55','\x4E\x05\x55','\x4E\x06\x55','\x4E\x07\x55','\x4E\x08\x55','\x4E\x09\x55','\x4E\x0A\x55','\x4E\x0B\x55','\x4E\x0C\x55','\x4E\x0D\x55','\x4E\x0E\x55','\x4E\x10\x55','\x4E\x11\x55','\x4E\x12\x55','\x4E\x13\x55','\x4E\x14\x55','\x4E\x15\x55','\x4E\x16\x55','\x4E\x17\x55','\x4E\x18\x55','\x4E\x19\x55','\x4E\x1A\x55','\x4E\x1B\x55','\x4E\x1B\x55','\x4E\x1B\x55']
-            for message in messages:
-                self.sendUdpCommand(self, message, timer)
+            if self.isMovie:
+                self.isMovie = 0
+                timer = 0.2
+                messages = ['\x4E\x04\x55','\x4E\x05\x55','\x4E\x06\x55','\x4E\x07\x55','\x4E\x08\x55','\x4E\x09\x55','\x4E\x0A\x55','\x4E\x0B\x55','\x4E\x0C\x55','\x4E\x0D\x55','\x4E\x0E\x55','\x4E\x10\x55','\x4E\x11\x55','\x4E\x12\x55','\x4E\x13\x55','\x4E\x14\x55','\x4E\x15\x55','\x4E\x16\x55','\x4E\x17\x55','\x4E\x18\x55','\x4E\x19\x55','\x4E\x1A\x55','\x4E\x1B\x55','\x4E\x1B\x55','\x4E\x1B\x55']
+                for message in messages:
+                    self.sendUdpCommand(self, message, timer)
 
         def sendUdpCommand(self, event, message, timer):
             UDP_IP = '192.168.1.2'
             UDP_PORT = 8899
-            print("sendUdpCommand UDP target IP:", UDP_IP)
-            print("sendUdpCommand UDP target port:", UDP_PORT)
-            print("sendUdpCommand message:", message)
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
             sock.sendto(message, (UDP_IP, UDP_PORT))
             time.sleep(timer)
